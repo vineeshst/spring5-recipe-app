8b47a5763e7ab86a07fc7fb71f451fdd4053fd79
vineeshst <vineeshstruts@gmail.com>
2019-12-10 20:24:12 +0100

    Unit of Measure added

--- src/main/java/guru/springframework/domain/Ingredient.java
+++ src/main/java/guru/springframework/domain/Ingredient.java
@@ -9,6 +9,8 @@ public class Ingredient {
     private Long Id;
     private String description;
     private BigDecimal amount;
+    @OneToOne(fetch = FetchType.EAGER)
+    private UnitOfMeasure uom;
     @ManyToOne
     private Recipe recipe;
 
