2c883e4311f28d661108b43bf70ed80c7fec5556
vineeshst <vineeshstruts@gmail.com>
2019-12-10 16:06:50 +0100

    One to One Relation Mapping

---
+++ src/main/java/guru/springframework/domain/Notes.java
@@ -0,0 +1,38 @@
+package guru.springframework.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Notes {
+    @Id
+    @GeneratedValue
+    private Long Id;
+    @OneToOne
+    private Recipe recipe;
+    @Lob
+    private String recipeNotes;
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public String getRecipeNotes() {
+        return recipeNotes;
+    }
+
+    public void setRecipeNotes(String recipeNotes) {
+        this.recipeNotes = recipeNotes;
+    }
+}
