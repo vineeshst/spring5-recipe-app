4ef4566b75571d984d5e3ca2886e9761b58659c7
vineeshst <vineeshstruts@gmail.com>
2019-12-11 08:22:57 +0100

    Many to Many relationship added

--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -25,6 +25,11 @@ public class Recipe {
 
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
+    @ManyToMany
+    @JoinTable(name = "recipe_category",
+    joinColumns = @JoinColumn(name = "recipe_id"),
+    inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categories;
 
     public Long getId() {
         return Id;
@@ -120,5 +125,13 @@ public class Recipe {
 
     public void setDifficulty(Difficulty difficulty) {
         this.difficulty = difficulty;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
     }
 }
